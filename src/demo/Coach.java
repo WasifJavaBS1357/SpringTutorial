537ace645fcfea0cdeb144c277b57fd39f16da1c
WasifJavaBS1357 <wasif.islam@Brainstation-23.com>
2023-03-28 21:10:23 +0600

    initiating

---
+++ src/demo/Coach.java
@@ -0,0 +1,12 @@
+package demo;
+
+import java.io.ObjectInputStream.GetField;
+
+/**
+ * @author BS1123
+ *
+ */
+public interface Coach {
+	public String getDailyWorkout();
+	public String getDailyFortune();
+}
